c8f468b2aa86aef077b5f94f3700d060ddffb5ca
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-10 13:39:53 -0300

    Ajustando o resolver de paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
+import path from "path";
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  alias: { "/@": path.resolve(__dirname, "src/") },
+  plugins: [reactRefresh()],
+});
