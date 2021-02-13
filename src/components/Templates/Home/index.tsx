6af0885a02e41fd95643eb50c923217ab242963e
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-13 14:49:55 -0300

    Atualizando página inicial

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -5,6 +5,8 @@ import { ColorHSL } from "/@/domain/model/color/ColorHSL";
 import { HexConvertor } from "/@/data/useCases/colorPallete/HexConvertor";
 import { ColorPalleteCreator } from "/@/data/useCases/colorPallete/ColorPalleteCreator";
 import Button from "/@/components/Atoms/Button";
+import TextField from "../../Atoms/TextField";
+import { Spacer } from "../../Atoms/Spacer";
 
 export default function Home() {
   const [showPallete, setShowPallete] = useState(false);
@@ -42,61 +44,70 @@ export default function Home() {
   return (
     <div>
       <header>
+        <h1>RoundATint</h1>
+        <h3>Um gerador de paleta de cores para o seu novo projeto</h3>
+      </header>
+
+      <Spacer size={24} />
+
+      <section>
         <form onSubmit={generateColorPallete}>
-          <input
-            type="text"
-            value={colorHex}
-            onChange={(e) => setColorHex(e.target.value)}
+          <TextField
+            id="colorChooser"
+            name="colorChooser"
+            initialValue={colorHex}
+            placeholder="Exemplo: #D33F49"
+            label="Digite o código HEX da cor"
+            onInputChange={(value: string) => setColorHex(value)}
           />
 
           <Button type="submit">Gerar Paleta de Cores</Button>
         </form>
-      </header>
 
-      <div>
-        {showPallete && (
-          <>
-            <div
-              style={{
-                display: "flex",
-              }}
-            >
-              {newShadeColors.map((color) => (
-                <div
-                  key={`hsl(${color.hue},${color.saturation},${color.lightness})`}
-                  style={{
-                    display: "block",
-                    width: "150px",
-                    height: "150px",
-                    margin: "12px",
-                    background: `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`,
-                  }}
-                />
-              ))}
-            </div>
+        <div>
+          {showPallete && (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                {newShadeColors.map((color) => (
+                  <div
+                    key={`hsl(${color.hue},${color.saturation},${color.lightness})`}
+                    style={{
+                      display: "block",
+                      width: "150px",
+                      height: "150px",
+                      margin: "12px",
+                      background: `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`,
+                    }}
+                  />
+                ))}
+              </div>
 
-            <div
-              style={{
-                display: "flex",
-              }}
-            >
-              {newTintColors.map((color) => (
-                <div
-                  key={`hsl(${color.hue},${color.saturation},${color.lightness})`}
-                  style={{
-                    display: "block",
-                    width: "150px",
-                    height: "150px",
-                    margin: "12px",
-                    background: `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`,
-                  }}
-                />
-              ))}
-            </div>
-          </>
-        )}
-      </div>
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                {newTintColors.map((color) => (
+                  <div
+                    key={`hsl(${color.hue},${color.saturation},${color.lightness})`}
+                    style={{
+                      display: "block",
+                      width: "150px",
+                      height: "150px",
+                      margin: "12px",
+                      background: `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`,
+                    }}
+                  />
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
-
