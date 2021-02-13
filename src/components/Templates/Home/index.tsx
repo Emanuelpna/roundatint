a283957534738374cf1927f6597d79b7f9bbf3f0
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-13 15:37:03 -0300

    Atualizando template da tela inicial

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -4,9 +4,11 @@ import { ColorHSL } from "/@/domain/model/color/ColorHSL";
 
 import { HexConvertor } from "/@/data/useCases/colorPallete/HexConvertor";
 import { ColorPalleteCreator } from "/@/data/useCases/colorPallete/ColorPalleteCreator";
+
 import Button from "/@/components/Atoms/Button";
-import TextField from "../../Atoms/TextField";
-import { Spacer } from "../../Atoms/Spacer";
+import { Spacer } from "/@/components/Atoms/Spacer";
+import TextField from "/@/components/Atoms/TextField";
+import ColorList from "/@/components/Molecules/ColorList";
 
 export default function Home() {
   const [showPallete, setShowPallete] = useState(false);
@@ -64,46 +66,18 @@ export default function Home() {
           <Button type="submit">Gerar Paleta de Cores</Button>
         </form>
 
+        <Spacer size={24} />
+
         <div>
           {showPallete && (
             <>
-              <div
-                style={{
-                  display: "flex",
-                }}
-              >
-                {newShadeColors.map((color) => (
-                  <div
-                    key={`hsl(${color.hue},${color.saturation},${color.lightness})`}
-                    style={{
-                      display: "block",
-                      width: "150px",
-                      height: "150px",
-                      margin: "12px",
-                      background: `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`,
-                    }}
-                  />
-                ))}
-              </div>
+              <Spacer size={24} />
 
-              <div
-                style={{
-                  display: "flex",
-                }}
-              >
-                {newTintColors.map((color) => (
-                  <div
-                    key={`hsl(${color.hue},${color.saturation},${color.lightness})`}
-                    style={{
-                      display: "block",
-                      width: "150px",
-                      height: "150px",
-                      margin: "12px",
-                      background: `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`,
-                    }}
-                  />
-                ))}
-              </div>
+              <ColorList title="Sombras" colorList={newShadeColors} />
+
+              <Spacer size={24} />
+
+              <ColorList title="Luzes" colorList={newTintColors} />
             </>
           )}
         </div>
