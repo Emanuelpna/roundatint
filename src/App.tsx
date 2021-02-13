4e87ee73d8fae0747a88c4fd0806df78693d2442
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-13 20:31:46 -0300

    Carregando páginas assincronamente e corrigindo slug

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import loadable from "@loadable/component";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Index from "/@/pages/Index";
+import Index from "/@/pages/index";
+
+const Projeto = loadable(() => import("/@/pages/projeto"), {
+  fallback: <div>Carregando...</div>,
+});
 
 import { theme } from "/@/styles/themes";
 import { GlobalStyles } from "/@/styles/global";
@@ -13,8 +18,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Index />
+          </Route>
+          <Route path="/projeto">
+            <Projeto />
           </Route>
         </Switch>
       </ThemeProvider>
