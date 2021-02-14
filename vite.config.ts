3c2b14785fe1f82f03161778d5ee20c6fb86abb5
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-14 00:52:54 -0300

    Configurando metatags e removendo plugins que não serão mais usados

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import compress from "vite-plugin-compress";
 import mdx from "@brillout/vite-plugin-mdx";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
   alias: { "/@": path.resolve(__dirname, "src/") },
-  plugins: [mdx(), VitePWA(), compress(), reactRefresh()],
+  plugins: [mdx(), reactRefresh()],
 });
