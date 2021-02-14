f2fd7edb24b70f806ddb28459c9739e6b22c2787
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-14 12:45:09 -0300

    Possibilitando usar color preview na linha de um texto

--- src/components/Atoms/ColorPreview/index.tsx
+++ src/components/Atoms/ColorPreview/index.tsx
@@ -4,8 +4,18 @@ import * as S from "./styles";
 
 type ColorPreviewProps = {
   colorHex: string;
+  className: string;
 };
 
-export default function ColorPreview({ colorHex }: ColorPreviewProps) {
-  return <S.ColorPreview title={colorHex} colorHex={colorHex} />;
+export default function ColorPreview({
+  colorHex,
+  className,
+}: ColorPreviewProps) {
+  return (
+    <S.ColorPreview
+      title={colorHex}
+      colorHex={colorHex}
+      className={className}
+    />
+  );
 }
--- src/components/Atoms/ColorPreview/styles.ts
+++ src/components/Atoms/ColorPreview/styles.ts
@@ -4,9 +4,16 @@ export const ColorPreview = styled.span<{ colorHex: string }>`
   ${({ theme, colorHex }) => css`
     width: 36px;
     height: 36px;
-    display: block;
+    display: inline-block;
     background: ${colorHex};
     border-radius: ${theme.border.radius};
     box-shadow: ${theme.shadows.elevation1};
+
+    &.insideText {
+      width: 28px;
+      height: 28px;
+      margin-left: -4px;
+      margin-bottom: -9px;
+    }
   `}
 `;
