666ccf9b763963fd941180f64fda1ef231b418ed
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-02-18 12:35:30 -0300

    Tornando site uma PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,43 @@
 import path from "path";
 import { defineConfig } from "vite";
 import mdx from "@brillout/vite-plugin-mdx";
+import viteImagemin from "vite-plugin-imagemin";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
-  plugins: [mdx(), reactRefresh()],
+  plugins: [
+    mdx(),
+    reactRefresh(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      webp: {
+        quality: 75,
+      },
+      mozjpeg: {
+        quality: 65,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+          {
+            removeEmptyAttrs: false,
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: { "/@": path.resolve(__dirname, "src/") },
   },
